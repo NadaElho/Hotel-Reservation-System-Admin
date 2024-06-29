ac5e4ada2fd7e92a491864a5a4b71c278ced5b64
samarali <engsamar105@gmail.com>
2024-06-29 04:43:08 +0300

    update

--- src/interceptor.js
+++ src/interceptor.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://hotel-reservation-system-node-1.onrender.com/api/v1",
 });
+
+// const axiosInstance = axios.create({
+//   baseURL: "http://localhost:3000/api/v1",
+// });
 try {
   const data = await axios.get(
-    `http://localhost:3000/api/v1/users/userProfile`,
+    `https://hotel-reservation-system-node-1.onrender.com/api/v1/users/userProfile`,
     {
       headers: {
         authorization: `Bearer ${localStorage.getItem("token")}`,
@@ -12,19 +16,14 @@ try {
     }
   );
   // console.log("data ", data);
-  axiosInstance.interceptors.request.use(
-    (config) => {
-      const accessToken = localStorage.getItem("token");
-      if (accessToken) {
-        if (config.headers)
-          config.headers.authorization = ` Bearer ${accessToken}`;
-      }
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
+  axiosInstance.interceptors.request.use((config) => {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken) {
+      if (config.headers)
+        config.headers.authorization = `Bearer ${accessToken}`;
     }
-  );
+    return config;
+  });
 } catch (err) {
   localStorage.removeItem("token");
   // console.log("err", err.response.data.message);
